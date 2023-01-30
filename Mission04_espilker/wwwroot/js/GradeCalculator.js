28ca4ea85157dbb4ad5d07de9306c120d0f0a2e9
espilker <ethanspilker@gmail.com>
2023-01-30 15:36:29 -0700

    Add project files.

---
+++ Mission04_espilker/wwwroot/js/GradeCalculator.js
@@ -0,0 +1,46 @@
+﻿$("#btnFinal").click(function () {
+
+
+    // Assign values from html page into variables
+    let iAssignment = parseFloat($("#txtAssignment").val());
+    let iProject = parseFloat($("#txtProject").val());
+    let iQuiz = parseFloat($("#txtQuiz").val());
+    let iMidterm = parseFloat($("#txtMidterm").val());
+    let iFinal = parseFloat($("#txtFinal").val());
+    let iIntex = parseFloat($("#txtIntex").val());
+    let ltrFinal = "A";
+
+    //calculate the total grade from the values
+    let PctFinalGrade = iAssignment + iProject + iQuiz + iMidterm + iFinal + iIntex;
+
+    //big if statement to determine what letter grade corresponds with the number grade
+    if (PctFinalGrade < 94) {
+        ltrFinal = "A-";
+    } else if (PctFinalGrade < 90) {
+        ltrFinal = "B+"
+    } else if (PctFinalGrade < 87) {
+        ltrFinal = "B"
+    } else if (PctFinalGrade < 84) {
+        ltrFinal = "B-"
+    } else if (PctFinalGrade < 80) {
+        ltrFinal = "C+"
+    } else if (PctFinalGrade < 77) {
+        ltrFinal = "C"
+    } else if (PctFinalGrade < 74) {
+        ltrFinal = "C-"
+    } else if (PctFinalGrade < 70) {
+        ltrFinal = "D+"
+    } else if (PctFinalGrade < 67) {
+        ltrFinal = "D"
+    } else if (PctFinalGrade < 64) {
+        ltrFinal = "D-"
+    } else if (PctFinalGrade < 60) {
+        ltrFinal = "E"
+    }
+
+    //assigns a label on the index page a string with the different calculated values
+    $("#textFinalGradeLetter").text("Grade: " + ltrFinal + "    " + PctFinalGrade + "%")
+
+});
+
+
